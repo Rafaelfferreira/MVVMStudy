fef1dc49d7a40f3101014ead1c34813a306a7bdb
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2020-07-06 16:29:43 -0300

    Created Project and movie model

---
+++ MVVMStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVMStudy
+//
+//  Created by Rafael Ferreira on 06/07/20.
+//  Copyright © 2020 Rafael Ferreira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
